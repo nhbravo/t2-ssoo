96663c349ba96a0f4aa6e9b63e2ca9a9ecc42f4f
Nicolás Bravo de la Cruz <nhbravo@uc.cl>
2022-04-05 19:04:20 -0400

    process struct

--- src/mlfq/main.c
+++ src/mlfq/main.c
@@ -14,14 +14,22 @@ int main(int argc, char const *argv[])
 	printf("Nombre archivo: %s\n", file_name);
 	printf("Cantidad de procesos: %d\n", input_file->len);
 	printf("Procesos:\n");
-	for (int i = 0; i < input_file->len; ++i)
-	{
-		for (int j = 0; j < 7; ++j)
-		{
-			printf("%s ", input_file->lines[i][j]);
-		}
-		printf("\n");
+	Process *processes[input_file->len];
+	for (int i = 0; i < input_file->len; ++i) {
+		Process *process = process_init(
+			atoi(input_file->lines[i][1]),
+			input_file->lines[i][0],
+			atoi(input_file->lines[i][2]),
+			atoi(input_file->lines[i][3]),
+			atoi(input_file->lines[i][4]),
+			atoi(input_file->lines[i][5]),
+			atoi(input_file->lines[i][6])
+		);
+		processes[i] = process;
 	}
 
+	for (int i = 0; i < input_file->len; ++i) {
+		process_destroy(processes[i]);
+	}
 	input_file_destroy(input_file);
 }
--- src/process/process.h
+++ src/process/process.h
@@ -1,1 +1,24 @@
 #pragma once
+#include <stdint.h>
+
+struct process;
+typedef struct process Process;
+
+struct process
+{
+  int32_t pid;
+  char *name;
+  int32_t priority;
+  enum state { RUNNING, READY, WAITING, FINISHED } state;
+  int32_t init_time;
+  int32_t current_time;
+  int32_t cycles;
+  int32_t wait;
+  int32_t waiting_delay;
+  int32_t s;
+};
+
+Process* process_init(
+  int32_t pid, char *name, int32_t init_time, int32_t cycles, int32_t wait, int32_t waiting_delay, int32_t s
+);
+void process_destroy(Process* process);
