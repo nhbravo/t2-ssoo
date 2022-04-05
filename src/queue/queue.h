d0c0dba57616dd9bcfea4f9bc262dca9f3b50066
fede-dealing <fede@dealingapp.com>
2022-04-05 19:12:44 -0400

    feat() queue struct

--- src/queue/queue.h
+++ src/queue/queue.h
@@ -1,1 +1,15 @@
 #pragma once
+#include "../process/process.h"
+
+struct queue;
+typedef struct queue Queue;
+
+struct queue {
+  int process_quantity;
+  Process** process;
+  int priority;
+  int quantum;
+};
+
+Queue* queue_init(int process_quenatity);
+void queue_destroy(Queue* queue);
