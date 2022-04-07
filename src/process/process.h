018d404fb5ebdf49991f946e1cae51e949063da5
fede-dealing <fede@dealingapp.com>
2022-04-07 16:44:47 -0400

    rebase

--- src/process/process.h
+++ src/process/process.h
@@ -11,10 +11,15 @@ struct process
   enum state { RUNNING, READY, WAITING, FINISHED, CREATED } state;
   int32_t init_time;
   int32_t enter_queue_time;
+  int32_t running_time;
   int32_t cycles;
   int32_t wait;
   int32_t waiting_delay;
   int32_t s;
+  int32_t in_cpu_count;
+  int32_t interrupt_count;
+  int32_t in_ready_count;
+  int32_t in_waiting_count;
 };
 
 Process* process_init(
