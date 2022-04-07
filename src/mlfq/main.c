6046a18f03c70fc895f70bff79f757e913056dfd
fede-dealing <fede@dealingapp.com>
2022-04-07 11:19:03 -0400

    build-heap

--- src/mlfq/main.c
+++ src/mlfq/main.c
@@ -14,7 +14,11 @@ int main(int argc, char const *argv[])
 	printf("Nombre archivo: %s\n", file_name);
 	printf("Cantidad de procesos: %d\n", input_file->len);
 	printf("Procesos:\n");
-	Process *processes[input_file->len];
+	Queue *queue_2 = queue_init(input_file->len, 2, 5);
+	Queue *queue_1 = queue_init(0, 1, 5);
+	Queue *queue_0 = queue_init(0, 0, 5);
+	int actual_tick = 0;
+
 	for (int i = 0; i < input_file->len; ++i) {
 		Process *process = process_init(
 			atoi(input_file->lines[i][1]),
@@ -25,11 +29,23 @@ int main(int argc, char const *argv[])
 			atoi(input_file->lines[i][5]),
 			atoi(input_file->lines[i][6])
 		);
-		processes[i] = process;
+		process_insert(queue_2, process);
 	}
 
-	for (int i = 0; i < input_file->len; ++i) {
-		process_destroy(processes[i]);
+	while (actual_tick >= 0) {
+		Process *actual_process = process_pop(queue_2);
+
+		printf("%i\n", actual_process->init_time);
+
+		if (!actual_process) {
+			actual_tick = -1;
+		} else {
+			actual_tick ++;
+		}
 	}
+
+	queue_destroy(queue_2);
+	queue_destroy(queue_1);
+	queue_destroy(queue_0);
 	input_file_destroy(input_file);
 }
